cb0694f22c584b340b6ecd19f9bf65e8e003fa65
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-01-21 14:33:31 +0530

    changes exp2

--- app.js
+++ app.js
@@ -6,6 +6,21 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) =>{
     res.render("exp1")
 } );
+app.get('/about', (req, res) =>{
+    res.render("exp2/about")
+} );
+app.get('/calendar', (req, res) =>{
+    res.render("exp2/calendar")
+} );
+app.get('/admission', (req, res) =>{
+    res.render("exp2/admission")
+} );
+app.get('/exp2', (req, res) =>{
+    res.render("exp2/exp2")
+} );
+app.post('/result', (req, res) =>{
+    res.render("exp2/result")
+} );
 
 app.listen(process.env.PORT,process.env.IP, function(){
 	console.log("hello");
