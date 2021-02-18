fcfe358267dd8c4e1116fe1c2f3e616efeddcfcf
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-02-18 12:28:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.post('/result', (req, res) =>{
     res.render("exp2/result")
 } );
 
-app.listen(3000, function(){
+app.listen(process.env.PORT,process.env.IP, function(){
 	console.log("hello");
 });
