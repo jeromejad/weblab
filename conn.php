8f5ea2949858d0e3267e1bb13a38933c77084d3c
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-03-09 14:00:21 +0530

    Update conn.php

--- conn.php
+++ conn.php
@@ -7,7 +7,7 @@ $db   = "bbwn7crtmngob6psyayn";
 $conn = null;
 
 try {
-  $conn = new PDO("mysql://usqjzrlnqnihzctu:51VPthtnMebwjrLd9jD1@bbwn7crtmngob6psyayn-mysql.services.clever-cloud.com:3306/bbwn7crtmngob6psyayn");
+  $conn = new PDO("mysql:host={$host};dbname={$db};",$user,$pass);
 } catch (Exception $e) {
   
 }
