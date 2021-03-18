2d0bb9ede7190e4ccb073b2d277f01835d37f3fe
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-03-18 16:57:32 +0530

    jbdcbdsu

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -1,21 +1,14 @@
 <?php
-define('HOST', 'bnjhz69tzqtptkhxejq4-mysql.services.clever-cloud.com');
-define('USER', 'uixodnli2w95t7oa');
-define('PASSWORD', 'WDZfqukULNtn9LzL9tJl');
-define('DATABASE_NAME', 'bnjhz69tzqtptkhxejq4');
 
-define('CURRENCY', '€');
-
+/**
+ * 
+ */
 class Database
 {
-	$servername = HOST;
-$username = USER;
-$password = PASSWORD;
-$db = DATABASE_NAME;
 	
 	private $con;
 	public function connect(){
-		$this->con = mysqli_connect($servername, $username, $password,$db);
+		$this->con = new Mysqli("bnjhz69tzqtptkhxejq4-mysql.services.clever-cloud.com", "uixodnli2w95t7oa", "WDZfqukULNtn9LzL9tJl", "bnjhz69tzqtptkhxejq4");
 		return $this->con;
 	}
 }
