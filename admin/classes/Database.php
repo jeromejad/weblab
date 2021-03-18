0128d7f12a0f64f2970c9a05c14d1ae72579908d
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-03-18 16:21:04 +0530

    sbvbuisb

--- admin/classes/Database.php
+++ admin/classes/Database.php
@@ -5,10 +5,14 @@
  */
 class Database
 {
+	$servername = 'bnjhz69tzqtptkhxejq4-mysql.services.clever-cloud.com';
+$username = 'uixodnli2w95t7oa';
+$password = 'WDZfqukULNtn9LzL9tJl';
+$db = 'bnjhz69tzqtptkhxejq4';
 	
 	private $con;
 	public function connect(){
-		$this->con = new Mysqli("localhost", "root", "", "JEROMEstore");
+		$this->con = mysqli_connect($servername, $username, $password,$db);
 		return $this->con;
 	}
 }
