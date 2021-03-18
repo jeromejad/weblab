459aa5143f6b3db79146766a2a77e67e3c8b0c85
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-03-18 15:33:10 +0530

    kvsnjxidbvis

---
+++ config/constants.php
@@ -0,0 +1,12 @@
+<?php
+
+define('HOST', 'bnjhz69tzqtptkhxejq4-mysql.services.clever-cloud.com');
+define('USER', 'uixodnli2w95t7oa');
+define('PASSWORD', 'WDZfqukULNtn9LzL9tJl');
+define('DATABASE_NAME', 'bnjhz69tzqtptkhxejq4');
+
+define('CURRENCY', '€');
+
+
+
+?>
