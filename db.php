e9b76430f6d5472c5ebac3342c426b63a435f787
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-04-04 12:48:48 +0530

    new

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+
+$db = new PDO("mysql:host=bnjhz69tzqtptkhxejq4-mysql.services.clever-cloud.com;dbname=bnjhz69tzqtptkhxejq4", "uixodnli2w95t7oa", "WDZfqukULNtn9LzL9tJl");
+
+
+?>
