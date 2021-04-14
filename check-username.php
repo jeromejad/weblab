f170d630055d64283f80fe6ebcf813189d9c9d13
jeromejad <77095320+jeromejad@users.noreply.github.com>
2021-04-14 12:51:33 +0530

    Update check-username.php

--- check-username.php
+++ check-username.php
@@ -1,13 +1,18 @@
 <?php
-
 $input = $_GET['name'];
 //open connection with mysql server
-
+$conn =mysqli_connect('bnjhz69tzqtptkhxejq4-mysql.services.clever-cloud.com', 'uixodnli2w95t7oa', 'WDZfqukULNtn9LzL9tJl' , 'bnjhz69tzqtptkhxejq4');
 if($input!=""){
-   
-      $hint="jerome";
-           
-          echo $hint==""  ?  "User name not exist!" : "User name already exist!"."<br/>".$hint;
+    
+    $result = $conn->query("SELECT * FROM tbl_registration WHERE first_name LIKE '$input' ");
+      $hint="";
+           while($a = mysqli_fetch_array($result))
+            {
+                   $hint=$hint.$a[0].'<br>';
+            }
+        
+           echo $hint==""  ?  "User name not exist!" : "User name already exist!"."<br/>".$hint;
 }
-
+mysqli_close($conn);
 ?>
+
